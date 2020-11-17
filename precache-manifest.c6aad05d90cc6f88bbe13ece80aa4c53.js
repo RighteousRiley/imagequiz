851c5800c9def8203632e368fe1dc26ff0a973ad
RighteousRiley <rcsimpso@gmail.com>
2020-11-16 17:33:04 -0700

    Updates

---
+++ precache-manifest.c6aad05d90cc6f88bbe13ece80aa4c53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0660ea402183e453602df66118ec6cbd",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "7974c0888bb44345d4e4",
+    "url": "/imagequiz/static/css/main.b03b839e.chunk.css"
+  },
+  {
+    "revision": "eefd3449119cf9d10820",
+    "url": "/imagequiz/static/js/2.69dc7f32.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/imagequiz/static/js/2.69dc7f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7974c0888bb44345d4e4",
+    "url": "/imagequiz/static/js/main.555be0c0.chunk.js"
+  },
+  {
+    "revision": "197e03e85427dd3240fd",
+    "url": "/imagequiz/static/js/runtime-main.b6bec1be.js"
+  },
+  {
+    "revision": "746d7a769b172642a8102c675ac14d37",
+    "url": "/imagequiz/static/media/animals.746d7a76.jpg"
+  },
+  {
+    "revision": "c64ea3c7b8d5a66aeb18305fe84305b5",
+    "url": "/imagequiz/static/media/colors.c64ea3c7.jpg"
+  },
+  {
+    "revision": "511f2e8613c46b9e3086570aba100ef4",
+    "url": "/imagequiz/static/media/flower.511f2e86.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43b82cdcb8f6083905a2c440a0d78bf3.js"
+  "/imagequiz/precache-manifest.c6aad05d90cc6f88bbe13ece80aa4c53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imagequiz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
